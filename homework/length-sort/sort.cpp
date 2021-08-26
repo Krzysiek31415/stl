abff479fcaa6442c86eb5063f7f947bc3665de5b
Krzysiek31415 <krzysztof.ciecko.kontakt@gmail.com>
2021-08-26 22:07:59 +0200

    sort ...

--- homework/length-sort/sort.cpp
+++ homework/length-sort/sort.cpp
@@ -4,7 +4,11 @@ std::deque<std::string>  lengthSort(std::forward_list<std::string> &wordFlist)
 {
     std::deque<std::string> wordDeque{};
     wordFlist.sort();
-    wordFlist.sort(cmp);
+    auto lambda = [](const std::string &a, const std::string &b){return (b.size() > a.size())?true:false;};
+    //wordFlist.sort(cmp);
+    // wordFlist.sort(
+    // [](const std::string &a, const std::string &b){return (b.size() > a.size())?true:false;});
+    wordFlist.sort(lambda);
     wordDeque.insert(wordDeque.begin(),wordFlist.begin(),wordFlist.end());
     return wordDeque;
 }

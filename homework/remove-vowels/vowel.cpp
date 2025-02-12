89ecc120e00180eb7f23e4d443a5ea1ca3c4a150
Krzysiek31415 <krzysztof.ciecko.kontakt@gmail.com>
2025-02-12 18:46:38 +0100

    test pass

---
+++ homework/remove-vowels/vowel.cpp
@@ -0,0 +1,19 @@
+#include "vowel.hpp"
+#include <string>
+#include <vector>
+#include <algorithm>
+
+bool isVowel(char ch)
+{
+    // Make the list of vowels
+    std::string str = "aeiouAEIOU";
+    return (str.find(ch) != std::string::npos);
+}
+
+void removeVowels(std::vector<std::string>& data)
+{
+    for(auto& line: data)
+    {
+        line.erase(std::remove_if(line.begin(), line.end(), isVowel), line.end());
+    }
+}
